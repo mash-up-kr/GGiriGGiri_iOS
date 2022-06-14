9ac81498e9bddaf89d906e6f01c3b8965d0ed8c6
AhnSangHoon <ansh0324@gmail.com>
2022-06-14 21:05:28 +0900

    :tada: initialize project

---
+++ GGiriGGiri/GGiriGGiri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GGiriGGiri
+//
+//  Created by AhnSangHoon on 2022/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
