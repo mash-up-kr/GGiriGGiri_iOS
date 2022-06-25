b65cfb469b3cf28de48fca056ae6cc9135d56671
sanghee <dev.sanghee@gmail.com>
2022-06-26 03:11:28 +0900

    :recycle: ViewController -> MainViewController로 변경 및 배경색 수정

--- Projects/App/Sources/MainViewController.swift
+++ Projects/App/Sources/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  GGiriGGiri
 //
 //  Created by AhnSangHoon on 2022/06/14.
@@ -7,17 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
     }
 }
--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = MainViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
