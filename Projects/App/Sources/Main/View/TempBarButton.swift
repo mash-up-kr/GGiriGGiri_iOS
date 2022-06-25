462afbd26966c1cd1b6f0866273e7ab2dc3e4345
sanghee <dev.sanghee@gmail.com>
2022-06-26 03:22:28 +0900

    :bulb: 임시로 사용할 뷰 컴포넌트 추가 (#11)

---
+++ Projects/App/Sources/Main/View/TempBarButton.swift
@@ -0,0 +1,29 @@
+//
+//  TempBarButton.swift
+//  GGiriGGiri
+//
+//  Created by 안상희 on 2022/06/25.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+final class TempBarButton {
+    private(set) var myButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("마이박스", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        button.sizeToFit()
+        return button
+    }()
+    
+    private(set) var notiButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("알림", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        button.sizeToFit()
+        return button
+    }()
+}
