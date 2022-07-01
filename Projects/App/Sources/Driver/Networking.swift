507110ed2324d62607ecb2f768c711d4db9d6047
AhnSangHoon <ansh0324@gmail.com>
2022-07-01 22:38:14 +0900

    :bulb: Networking request 작성
    -- request를 위한 기본적인 틀 구현

--- Projects/App/Sources/Driver/Networking.swift
+++ Projects/App/Sources/Driver/Networking.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+import Alamofire
+import RxSwift
+
+final class Networking {
+    typealias Response<T: Decodable> = Result<T, AFError>
+    
+    static func request<T: Decodable>(type: T.Type) -> Single<Result<T, AFError>> {
+        .create { single in
+            AF.request("https://m.naver.com").response { [single] response in
+                if let error = response.error {
+                    single(.failure(error))
+                }
+                
+                guard let data = response.data else {
+                    single(
+                        .failure(
+                            AFError.responseSerializationFailed(reason: .inputFileNil)
+                        )
+                    )
+                    return
+                }
+                
+                do {
+                    let deocodedData = try JSONDecoder().decode(T.self, from: data)
+                    single(.success(.success(deocodedData)))
+                } catch {
+                    single(
+                        .failure(
+                            AFError.responseSerializationFailed(reason: .decodingFailed(error: error))
+                        )
+                    )
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
