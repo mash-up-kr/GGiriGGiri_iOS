a89e7db261ae50887d32f151fc69379ef3f1b717
Eddy <kimkyunghun3@naver.com>
2022-07-02 23:03:18 +0900

    :bulb: ToastView 구현 #5

---
+++ Projects/DesignSystem/Sources/Component/ToastView/DDIPToastView.swift
@@ -0,0 +1,87 @@
+//
+//  DDIPToastView.swift
+//  DesignSystem
+//
+//  Created by Eddy on 2022/07/02.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+import PinLayout
+
+public class DDIPToastView: UIView {
+    public let style: DDIPToastViewStyle
+    
+    public let titleLabel: UILabel = {
+        let titlelabel = UILabel()
+        titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        titlelabel.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        return titlelabel
+    }()
+    
+    public let iconImageView: UIImageView = {
+        let iconImageView = UIImageView()
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return iconImageView
+    }()
+    
+    public let descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
+        
+        return descriptionLabel
+    }()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public init(frame: CGRect = .zero, style: DDIPToastViewStyle) {
+        self.style = style
+        super.init(frame: .zero)
+        setUI()
+    }
+    
+    private func setUI() {
+        self.titleLabel.text = self.style.title.rawValue
+        self.descriptionLabel.text = setDescriptionLabel()
+        self.iconImageView.image = UIImage(systemName: self.style.imageIcon)
+        
+        self.backgroundColor = .gray
+        self.layer.cornerRadius = 12
+        
+        self.addSubview(titleLabel)
+        self.addSubview(iconImageView)
+        self.addSubview(descriptionLabel)
+        
+        setToastViewLayout()
+    }
+    
+    public func setDescriptionLabel() -> String {
+        let text = self.style.description.rawValue.components(separatedBy: "!")
+            return text[0] + "!" + "\n" + text[1]
+    }
+    
+    private func setToastViewLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 111.5),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -111.5),
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 28),
+            titleLabel.bottomAnchor.constraint(equalTo: self.iconImageView.topAnchor, constant: -16),
+            titleLabel.heightAnchor.constraint(equalToConstant: 24),
+            
+            iconImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            iconImageView.bottomAnchor.constraint(equalTo: self.descriptionLabel.topAnchor, constant: -16),
+            iconImageView.widthAnchor.constraint(equalToConstant: 128),
+            iconImageView.heightAnchor.constraint(equalToConstant: 128),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            descriptionLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -28)
+        ])
+    }
+}
