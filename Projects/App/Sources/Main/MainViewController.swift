df74bb87cd53b801f776814ea689cbe3f6465803
sanghee <dev.sanghee@gmail.com>
2022-07-07 02:11:42 +0900

    :bulb: 메인에서 최초로 + 버튼 클릭 시 갤러리 접근 권한을 요청하도록 구현 (#29)

--- Projects/App/Sources/Main/MainViewController.swift
+++ Projects/App/Sources/Main/MainViewController.swift
@@ -5,6 +5,7 @@
 //  Created by AhnSangHoon on 2022/06/14.
 //  Copyright © 2022 dvHuni. All rights reserved.
 
+import Photos
 import UIKit
 
 final class MainViewController: UIViewController {
@@ -64,5 +65,73 @@ final class MainViewController: UIViewController {
     
     @objc private func addButtonDidTapped() {
         // TODO: + 버튼 탭 되었을 때의 액션 구현
+        requestPHPhotoLibraryAuthorization {
+        }
+    }
+    
+    private func requestPHPhotoLibraryAuthorization(completion: @escaping () -> Void) {
+        if #available(iOS 14, *) {
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { authorizationStatus in
+                switch authorizationStatus {
+                case .notDetermined:
+                    DispatchQueue.main.async {
+                        self.alert(message: "갤러리 접근 권한이 설정되지 않았습니다.")
+                        completion()
+                    }
+                case .restricted:
+                    DispatchQueue.main.async {
+                        self.alert(message: "라이브러리 권한에 제한이 생겼습니다.")
+                        completion()
+                    }
+                case .denied:
+                    DispatchQueue.main.async {
+                        self.alert(message: "갤러리 접근 권한이 거부되었습니다.")
+                        completion()
+                    }
+                case .authorized:
+                    debugPrint("authorized")
+                    completion()
+                case .limited:
+                    debugPrint("limited")
+                    completion()
+                @unknown default:
+                    DispatchQueue.main.async {
+                        self.alert(message: "관리자에게 문의하세요.")
+                        completion()
+                    }
+                }
+            }
+        } else {
+            PHPhotoLibrary.requestAuthorization { authorizationStatus in
+                switch authorizationStatus {
+                case .notDetermined:
+                    DispatchQueue.main.async {
+                        self.alert(message: "갤러리 접근 권한이 설정되지 않았습니다.")
+                        completion()
+                    }
+                case .restricted:
+                    DispatchQueue.main.async {
+                        self.alert(message: "라이브러리 권한에 제한이 생겼습니다.")
+                        completion()
+                    }
+                case .denied:
+                    DispatchQueue.main.async {
+                        self.alert(message: "갤러리 접근 권한이 거부되었습니다.")
+                        completion()
+                    }
+                case .authorized:
+                    debugPrint("authorized")
+                    completion()
+                case .limited:
+                    debugPrint("limited")
+                    completion()
+                @unknown default:
+                    DispatchQueue.main.async {
+                        self.alert(message: "관리자에게 문의하세요.")
+                        completion()
+                    }
+                }
+            }
+        }
     }
 }
