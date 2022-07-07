c26d1ea66a1438104405f7cfec6eb489a829321b
Eddy <kimkyunghun3@naver.com>
2022-07-08 00:49:19 +0900

    :bulb: 응모박스 CardListButton 구현 #5
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 세부 내용을 아랫줄에 작성
    
    ################
    # 추가적인 세부 내용을 아랫줄에 작성
    
    ################
    # :tada: = 프로젝트 시작
    # :sparkles: = create 파일 생성 시
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시
    # :rocket: = release 배포 시
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우)
    # :memo: = 문서 작성 시
    # :art: = 디자인 시스템 관련 내용 (Color / Asset)
    # :hammer: = 테스트 코드 작성
    ################

---
+++ Projects/DesignSystem/Sources/Component/Button/DDIPCardListButton.swift
@@ -0,0 +1,41 @@
+//
+//  DDIPCardListButton.swift
+//  DesignSystem
+//
+//  Created by Eddy on 2022/07/02.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+public class DDIPCardListButton: UIButton {
+    public let style: DDIPCardListButtonStyle
+    
+    public init(frame: CGRect = .zero, style: DDIPCardListButtonStyle) {
+        self.style = style
+        super.init(frame: frame)
+        setButton()
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setButton() {
+        self.layer.cornerRadius = 12
+        self.setTitleColor(self.style.titleColor, for: .normal)
+        self.backgroundColor = self.style.buttonColor
+        self.titleLabel?.font = .systemFont(ofSize: 14)
+        self.setTitle(self.style.title?.rawValue, for: .normal)
+        self.titleLabel?.widthAnchor.constraint(equalToConstant: 52).isActive = true
+        self.titleLabel?.textAlignment = .center
+    }
+    
+    private func setUI() {
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 34),
+            self.widthAnchor.constraint(equalToConstant: 96)
+        ])
+    }
+}
