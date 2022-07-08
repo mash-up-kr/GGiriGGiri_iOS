b3c7c2b985ee9352d613c399a85746b26cb4c51b
sanghee <dev.sanghee@gmail.com>
2022-07-09 03:23:06 +0900

    :sparkles: RegisterViewController, RegisterView 추가 (#29)
    - 기프티콘 등록 화면 구현할 예정

---
+++ Projects/App/Sources/Register/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  GGiriGGiri
+//
+//  Created by 안상희 on 2022/07/09.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+final class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Projects/App/Sources/Register/View/RegisterView.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterView.swift
+//  GGiriGGiri
+//
+//  Created by 안상희 on 2022/07/09.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+final class RegisterView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setUI()
+    }
+    
+    private func setUI() {
+        backgroundColor = .white
+    }
+}
