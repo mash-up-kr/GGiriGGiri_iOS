01c3c2de57b262714f05b2bc85f5e8387a9d0a88
sanghee <dev.sanghee@gmail.com>
2022-07-16 03:51:55 +0900

    :bulb: 기프티콘 등록 화면 - 뿌리기 시간 PickerView 구현 (#35)

--- Projects/App/Sources/Register/View/RegisterGifticonDDipInfoView.swift
+++ Projects/App/Sources/Register/View/RegisterGifticonDDipInfoView.swift
@@ -8,25 +8,119 @@
 
 import UIKit
 
+import SnapKit
+
 final class RegisterGifticonDDipInfoView: BaseView {
     
     private let titleLabel = TempLabel(color: .black,
                                        text: "뿌리기 정보",
                                        font: .systemFont(ofSize: 16))
+    
+    private let ddipTitleLabel = TempLabel(color: .black,
+                                          text: "마감시간",
+                                          font: .systemFont(ofSize: 14))
+    
+//    private let timePickerView = UIPickerView()
+    private let timeInputView = TimeInputView()
+    
     override func setLayout() {
         super.setLayout()
         
         addSubview(titleLabel)
+        addSubview(ddipTitleLabel)
+        addSubview(timeInputView)
+//        addSubview(timePickerView)
         
         titleLabel.snp.makeConstraints {
             $0.top.leading.equalToSuperview()
             $0.height.equalTo(20)
+        }
+        
+        ddipTitleLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+            $0.height.equalTo(20)
+        }
+        
+        timeInputView.snp.makeConstraints {
+            $0.top.equalTo(ddipTitleLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(54)
+        }
+    }
+    
+    override func configure() {
+        super.configure()
+//
+//        timePickerView.dataSource = self
+//        timePickerView.delegate = self
+    }
+    
+    func configure(with title: String, placeholder: String) {
+//        ddipLabel.text = title
+        
+    }
+}
+
+fileprivate class TimeInputView: BaseView {
+    
+    private var timeList = ["30분", "1시간", "2시간", "3시간", "4시간", "5시간", "6시간", "7시간"]
+    
+    private let timePickerView = UIPickerView()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        timePickerView.subviews[1].backgroundColor = .clear
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        addSubview(timePickerView)
+        
+        timePickerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
     
     override func configure() {
         super.configure()
         
-        backgroundColor = .yellow
+        timePickerView.dataSource = self
+        timePickerView.delegate = self
+        
+        self.layer.cornerRadius = 12
+        backgroundColor = UIColor(red: 251.0 / 256, green: 251.0 / 256, blue: 251.0 / 256, alpha: 1)
     }
+}
+
+
+extension TimeInputView: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timeList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let pickerContainerView = UIView()
+        let pickerLabel = UILabel()
+        pickerLabel.font = UIFont.systemFont(ofSize: 23)
+        pickerLabel.textColor = UIColor.black
+        pickerLabel.backgroundColor = .clear
+        pickerLabel.textAlignment = .left
+        pickerLabel.text = "\(timeList[row])"
+        pickerContainerView.addSubview(pickerLabel)
+        pickerLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        return pickerContainerView
+    }
+}
+
+extension TimeInputView: UIPickerViewDelegate {
+    
 }
