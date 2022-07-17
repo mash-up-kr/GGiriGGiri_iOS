4cd11ef60b97684e418e5c4558d607aa0bb698c8
Eddy <kimkyunghun3@naver.com>
2022-07-17 16:08:01 +0900

    :bulb: ListCardView 구현
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

---
+++ Projects/DesignSystem/Sources/Component/ListCard/DDIPListCardView.swift
@@ -0,0 +1,152 @@
+//
+//  DDIPListCardView.swift
+//  DesignSystem
+//
+//  Created by Eddy on 2022/07/17.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+public class DDIPListCardView: UIView, AddViewsable {
+    public let style: DDIPListCardViewStyle
+    private let alarmButton: DDIPCardListButton
+    private let viewer: DDIPListViewer
+    
+    public let infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+                                    
+        return stackView
+    }()
+    
+    public let drawStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        return stackView
+    }()
+    
+    public let nameLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    public let brandLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    public let expirationLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    public let imageIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    public let dashedLine: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    public let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .right
+        
+        return label
+    }()
+    
+    public init(
+        frame: CGRect = .zero,
+        style: DDIPListCardViewStyle,
+        alarmButton: DDIPCardListButton,
+        viewer: DDIPListViewer
+    ) {
+        self.style = style
+        self.alarmButton = alarmButton
+        self.viewer = viewer
+        super.init(frame: frame)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.cornerRadius = 8
+        setView()
+        setUI()
+        setValue()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setView() {
+        viewer.translatesAutoresizingMaskIntoConstraints = false
+        alarmButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubViews([infoStackView, imageIcon, viewer, dashedLine, drawStackView])
+        infoStackView.addArrangedSubviews(with: [brandLabel, nameLabel, expirationLabel])
+        drawStackView.addArrangedSubviews(with: [alarmButton, descriptionLabel])
+        dashedLine.makeDashedBorderLine(color: .blue, strokeLength: 8, gapLength: 2, width: 1)
+    }
+    
+    private func setValue() {
+        brandLabel.text = style.brand
+        nameLabel.text = style.name
+        expirationLabel.text = style.expirationDate
+        imageIcon.image = UIImage(systemName: style.iconImage)
+        descriptionLabel.text = style.description
+    }
+    
+    private func setUI() {
+        NSLayoutConstraint.activate([
+            infoStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            infoStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            infoStackView.trailingAnchor.constraint(equalTo: imageIcon.leadingAnchor, constant: -17),
+            infoStackView.bottomAnchor.constraint(equalTo: dashedLine.topAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            imageIcon.topAnchor.constraint(equalTo: self.topAnchor, constant: 13),
+            imageIcon.trailingAnchor.constraint(equalTo: viewer.leadingAnchor),
+            imageIcon.bottomAnchor.constraint(equalTo: dashedLine.topAnchor, constant: -10)
+        ])
+        
+        NSLayoutConstraint.activate([
+            viewer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -9),
+            viewer.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            viewer.bottomAnchor.constraint(equalTo: dashedLine.topAnchor, constant: -68)
+        ])
+        
+        NSLayoutConstraint.activate([
+            dashedLine.heightAnchor.constraint(equalToConstant: 1),
+            dashedLine.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            dashedLine.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            dashedLine.bottomAnchor.constraint(equalTo: drawStackView.topAnchor, constant: -24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            drawStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            drawStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            drawStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -24)
+        ])
+    }
+}
+
