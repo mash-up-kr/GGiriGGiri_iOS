6dc58e3280fad4501aa0fab5860bdc59b5868de4
sanghee <dev.sanghee@gmail.com>
2022-07-20 18:49:29 +0900

    :bulb: 기프티콘 원본 보기 뷰 background 색상 변경 (#62)

--- Projects/App/Sources/Register/GiftionImageViewController.swift
+++ Projects/App/Sources/Register/GiftionImageViewController.swift
@@ -30,6 +30,8 @@ final class GiftionImageViewController: UIViewController {
     }
     
     private func configure() {
+        view.backgroundColor = .white
+        
         giftionImageView.contentMode = .scaleAspectFit
     }
 }
