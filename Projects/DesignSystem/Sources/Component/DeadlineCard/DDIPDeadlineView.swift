4009e3320ae3a85b2d84170a5d32bfa5b25cb7bd
Eddy <kimkyunghun3@naver.com>
2022-07-24 02:24:08 +0900

    :recycle: spaceView 구현 및 생성자 방식 변경
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

--- Projects/DesignSystem/Sources/Component/DeadlineCard/DDIPDeadlineView.swift
+++ Projects/DesignSystem/Sources/Component/DeadlineCard/DDIPDeadlineView.swift
@@ -13,6 +13,12 @@ public class DDIPDeadlineView: UIView, AddViewsable {
     private let CTAButton: DDIPCTAButton
     private let viwer: DDIPListViewer
     
+    public let imageIcon = UIImageView()
+    public let nameLabel = UILabel()
+    public let brandLabel = UILabel()
+    public let expirationLabel = UILabel()
+    public let dashedLine = UIView()
+    
     public let infoStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -22,13 +28,6 @@ public class DDIPDeadlineView: UIView, AddViewsable {
         stackView.spacing = 8
                                     
         return stackView
-    }()
-    
-    public let imageIcon: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return imageView
     }()
     
     public let timeStackView: UIStackView = {
@@ -86,57 +85,57 @@ public class DDIPDeadlineView: UIView, AddViewsable {
         return label
     }()
     
-    public let nameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    public let brandLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    public let expirationLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    public let dashedLine: UIView = {
+    public let spaceLeftView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
         
         return view
     }()
     
+    public let spaceRightView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        
+        return view
+    }()
+        
     public init(frame: CGRect = .zero, style: DDIPDeadlineViewStyle, button: DDIPCTAButton, viwer: DDIPListViewer) {
         self.style = style
         self.CTAButton = button
         self.viwer = viwer
         super.init(frame: frame)
-        self.layer.borderWidth = 2
-        self.layer.borderColor = UIColor.red.cgColor
-        self.layer.cornerRadius = 12
-        
-        setViews()
+        setView()
         setUI()
+        setValue()
+        setLeftSpaceView()
+        setRightSpaceView()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setViews() {
-        self.addSubViews([timeStackView, imageIcon, viwer, CTAButton, infoStackView, dashedLine])
+    private func setView() {
+        viwer.translatesAutoresizingMaskIntoConstraints = false
+        imageIcon.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        brandLabel.translatesAutoresizingMaskIntoConstraints = false
+        expirationLabel.translatesAutoresizingMaskIntoConstraints = false
+        dashedLine.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubViews([timeStackView, imageIcon, viwer, CTAButton, infoStackView, dashedLine, spaceLeftView, spaceRightView])
         infoStackView.addArrangedSubviews(with: [brandLabel, nameLabel, expirationLabel])
         timeStackView.addArrangedSubviews(with: [firstTimeLabel, secondTimeLabel, separatorLabel, firstMinuteLabel, secondMinuteLabel])
         
-        viwer.translatesAutoresizingMaskIntoConstraints = false
+        dashedLine.createDottedLine(width: 1, color: UIColor.black.cgColor)
+    }
+    
+    private func setValue() {
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.cornerRadius = 12
         
         imageIcon.image = UIImage(systemName: style.iconImage)
         nameLabel.text = style.name
@@ -147,8 +146,22 @@ public class DDIPDeadlineView: UIView, AddViewsable {
         secondTimeLabel.text = style.time
         firstMinuteLabel.text = style.time
         secondMinuteLabel.text = style.time
-        
-        dashedLine.makeDashedBorderLine(color: .black, strokeLength: 10, gapLength: 5, width: 1)
+    }
+    
+    private func setLeftSpaceView() {
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.bounds.width, y: self.bounds.height + 7), radius: 12, startAngle: .pi * 3/2, endAngle: .pi / 2, clockwise: true)
+        let circleShape = CAShapeLayer()
+        circleShape.fillColor = UIColor.red.cgColor
+        circleShape.path = circlePath.cgPath
+        spaceLeftView.layer.addSublayer(circleShape)
+    }
+    
+    private func setRightSpaceView() {
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.bounds.width + 18, y: self.bounds.height + 7), radius: 12, startAngle: .pi / 2, endAngle: .pi * 3/2, clockwise: true)
+        let circleShape = CAShapeLayer()
+        circleShape.fillColor = UIColor.red.cgColor
+        circleShape.path = circlePath.cgPath
+        spaceRightView.layer.addSublayer(circleShape)
     }
 
     private func setUI() {
@@ -174,6 +187,18 @@ public class DDIPDeadlineView: UIView, AddViewsable {
         ])
         
         NSLayoutConstraint.activate([
+            spaceLeftView.heightAnchor.constraint(equalToConstant: 18),
+            spaceLeftView.widthAnchor.constraint(equalToConstant: 18),
+            spaceLeftView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            spaceLeftView.bottomAnchor.constraint(equalTo: infoStackView.topAnchor, constant: -7),
+            
+            spaceRightView.heightAnchor.constraint(equalToConstant: 18),
+            spaceRightView.widthAnchor.constraint(equalToConstant: 18),
+            spaceRightView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            spaceRightView.bottomAnchor.constraint(equalTo: infoStackView.topAnchor, constant: -7),
+        ])
+        
+        NSLayoutConstraint.activate([
             infoStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
             infoStackView.topAnchor.constraint(equalTo: dashedLine.bottomAnchor, constant: 16),
             infoStackView.trailingAnchor.constraint(equalTo: viwer.leadingAnchor, constant: -28),
@@ -191,21 +216,5 @@ public class DDIPDeadlineView: UIView, AddViewsable {
             CTAButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
             CTAButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -24),
         ])
-    }
-}
-
-extension UIView {
-    func makeDashedBorderLine(color: UIColor, strokeLength: NSNumber, gapLength: NSNumber, width: CGFloat) {
-        let path = CGMutablePath()
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.lineWidth = width
-        shapeLayer.strokeColor = color.cgColor
-        shapeLayer.lineDashPattern = [strokeLength, gapLength]
-    
-        path.addLines(between: [CGPoint(x: bounds.minX, y: bounds.midY),
-                                CGPoint(x: bounds.maxX, y: bounds.midY)])
-        
-        shapeLayer.path = path
-        layer.addSublayer(shapeLayer)
     }
 }
