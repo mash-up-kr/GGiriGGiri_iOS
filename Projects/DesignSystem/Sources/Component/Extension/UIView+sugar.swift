aaf6f26c8ea6275d3798dcc441fc72a38c68eac6
Eddy <kimkyunghun3@naver.com>
2022-07-24 02:07:38 +0900

    :recycle: 점선 그리는 extension 파일 분리
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

---
+++ Projects/DesignSystem/Sources/Component/Extension/UIView+sugar.swift
@@ -0,0 +1,23 @@
+//
+//  UIView+sugar.swift
+//  DesignSystem
+//
+//  Created by Eddy on 2022/07/24.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func createDottedLine(width: CGFloat, color: CGColor) {
+        let caShapeLayer = CAShapeLayer()
+        caShapeLayer.strokeColor = color
+        caShapeLayer.lineWidth = width
+        caShapeLayer.lineDashPattern = [2,4]
+        let cgPath = CGMutablePath()
+        let cgPoint = [CGPoint(x: self.bounds.minX, y: 0), CGPoint(x: 310, y: 0)]
+        cgPath.addLines(between: cgPoint)
+        caShapeLayer.path = cgPath
+        layer.addSublayer(caShapeLayer)
+    }
+}
--- Projects/DesignSystem/Sources/Component/ListCard/DDIPListCardView.swift
+++ Projects/DesignSystem/Sources/Component/ListCard/DDIPListCardView.swift
@@ -165,17 +165,3 @@ public class DDIPListCardView: UIView, AddViewsable {
         ])
     }
 }
-
-extension UIView {
-    func createDottedLine(width: CGFloat, color: CGColor) {
-        let caShapeLayer = CAShapeLayer()
-        caShapeLayer.strokeColor = color
-        caShapeLayer.lineWidth = width
-        caShapeLayer.lineDashPattern = [2,4]
-        let cgPath = CGMutablePath()
-        let cgPoint = [CGPoint(x: self.bounds.minX, y: 0), CGPoint(x: 310, y: 0)]
-        cgPath.addLines(between: cgPoint)
-        caShapeLayer.path = cgPath
-        layer.addSublayer(caShapeLayer)
-    }
-}
