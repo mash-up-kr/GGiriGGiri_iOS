6608725b33ed34c7d3c5d258bb445b59d008bdd1
AhnSangHoon <ansh0324@gmail.com>
2022-07-25 21:02:23 +0900

    :refactor: ViewController 리팩터링
    -- ViewController 에서 사용되는 BusinessLogic ViewModel로 이동

--- Projects/App/Sources/Main/MainViewController.swift
+++ Projects/App/Sources/Main/MainViewController.swift
@@ -5,7 +5,6 @@
 //  Created by AhnSangHoon on 2022/06/14.
 //  Copyright © 2022 dvHuni. All rights reserved.
 
-import PhotosUI
 import UIKit
 
 import DesignSystem
@@ -34,6 +33,8 @@ final class MainViewController: BaseViewController<MainViewModelProtocol> {
         collectionView.configureDelegate(viewModel.collectionViewDelegate)
         
         floatingButton.addTarget(self, action: #selector(addButtonDidTapped), for: .touchUpInside)
+        
+        bind()
     }
     
     override func setLayout() {
@@ -73,83 +74,44 @@ final class MainViewController: BaseViewController<MainViewModelProtocol> {
         }
     }
     
+    private func bind() {
+        viewModel.alert = { [weak self] in
+            switch $0 {
+            case let .normal(message):
+                self?.alert(message: message)
+            case let .ok(message, title, handler):
+                self?.alert(
+                    message: message,
+                    okTitle: title,
+                    okHandler: handler
+                )
+            }
+        }
+        viewModel.present = { [weak self] in
+            self?.present(type: $0)
+        }
+        viewModel.selectedImage = { [weak self] image in
+            self?.alert(message: "쿠폰 이미지 분석 중~", okHandler: { _ in
+                let registerGifticonViewController = RegisterGifticonViewController()
+                registerGifticonViewController.giftionImage = image
+                registerGifticonViewController.modalPresentationStyle = .fullScreen
+                self?.present(registerGifticonViewController, animated: true)
+            })
+        }
+    }
+    
     @objc private func addButtonDidTapped() {
-        let authorizationStatus: PHAuthorizationStatus
-        authorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
-        
-        handleAuthorizationStatus(with: authorizationStatus)
+        viewModel.registerAction()
     }
     
-    /// 사용자에게 접근 권한을 요청하는 메서드
     private func requestPHPhotoLibraryAuthorization() {
-        PHPhotoLibrary.requestAuthorization(for: .readWrite) { authorizationStatus in
-            self.handleAuthorizationStatus(with: authorizationStatus)
-        }
+        viewModel.requestAuthorization()
     }
     
-    /// 현재 PHAuthorizationStatus에 따라 분기해서 처리하는 메서드
-    private func handleAuthorizationStatus(with authorizationStatus: PHAuthorizationStatus) {
-        switch authorizationStatus {
-        case .notDetermined:
-            requestPHPhotoLibraryAuthorization()
-        case .restricted:
-            DispatchQueue.main.async {
-                self.alert(message: "라이브러리 권한이 제한되어있습니다.")
-            }
-        case .denied:
-            DispatchQueue.main.async {
-                self.alert(message: "갤러리 접근 권한이 거부되었습니다.", okTitle: "권한 설정하러 가기") { _ in
-                    self.dismiss(animated: true)
-                } okHandler: { _ in
-                    if let url = URL(string: UIApplication.openSettingsURLString) {
-                        if UIApplication.shared.canOpenURL(url) {
-                            UIApplication.shared.open(url)
-                        }
-                    }
-                }
-            }
-        case .authorized:
-            presentPhotoPicker()
-            debugPrint("authorized")
-        case .limited:
-            debugPrint("limited")
-        @unknown default:
-            DispatchQueue.main.async {
-                self.alert(message: "관리자에게 문의하세요.")
-            }
+    private func present(type: MainViewModel.Present) {
+        switch type {
+        case let .picker(controller):
+            present(controller, animated: true)
         }
-    }
-    
-    private func presentPhotoPicker() {
-        var configuration = PHPickerConfiguration()
-        configuration.filter = .images
-
-        let picker = PHPickerViewController(configuration: configuration)
-        picker.delegate = self
-        present(picker, animated: true)
-    }
-}
-
-extension MainViewController: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-
-        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
-            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-                DispatchQueue.main.async {
-                    guard let self = self, let image = image as? UIImage else {
-                        debugPrint("could not load image", error?.localizedDescription ?? "")
-                        return
-                    }
-                    
-                    self.alert(message: "쿠폰 이미지 분석 중~", okHandler: { _ in
-                        let registerGifticonViewController = RegisterGifticonViewController()
-                        registerGifticonViewController.giftionImage = image
-                        registerGifticonViewController.modalPresentationStyle = .fullScreen
-                        self.present(registerGifticonViewController, animated: true)
-                    })
-                }
-            }
-        }
-        dismiss(animated: true)
     }
 }
--- Projects/App/Sources/Main/MainViewModel.swift
+++ Projects/App/Sources/Main/MainViewModel.swift
@@ -6,20 +6,106 @@
 //  Copyright © 2022 dvHuni. All rights reserved.
 //
 
+import PhotosUI
 import UIKit
 
 /// ViewModel에서 사용될 property와 method 정의
 protocol MainViewModelProtocol {
+    var alert: ((MainViewModel.AlertType) -> ())? { get set }
+    var present: ((MainViewModel.Present) -> ())? { get set }
+    var selectedImage: ((UIImage) -> ())? { get set }
     var collectionViewDataSource: MainCollectionViewDataSource { get }
     var collectionViewDelegate: MainCollectionViewDelegate { get }
     
-    func doSomething()
+    func registerAction()
+    func requestAuthorization()
+    func handleAuthorizationStatus(status: PHAuthorizationStatus)
 }
 
 /// ViewModelProtocol 구현
 final class MainViewModel: MainViewModelProtocol {
+    enum AlertType {
+        case normal(message: String)
+        case ok(message: String, title: String, handler: ((UIAlertAction) -> ())? = nil)
+    }
+    enum Present {
+        case picker(PHPickerViewController)
+    }
+    
+    var alert: ((AlertType) -> ())? = nil
+    var present: ((Present) -> ())? = nil
+    var selectedImage: ((UIImage) -> ())? = nil
+    
     let collectionViewDataSource = MainCollectionViewDataSource()
     let collectionViewDelegate = MainCollectionViewDelegate()
     
-    func doSomething() { }
+    func registerAction() {
+        let authorizationStatus: PHAuthorizationStatus
+        authorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        handleAuthorizationStatus(status: authorizationStatus)
+    }
+    
+    func requestAuthorization() {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { [weak self] in
+            self?.handleAuthorizationStatus(status: $0)
+        }
+    }
+    
+    func handleAuthorizationStatus(status: PHAuthorizationStatus) {
+        DispatchQueue.main.async { [weak self] in
+            switch status {
+            case .notDetermined:
+                self?.requestAuthorization()
+            case .restricted:
+                self?.alert?(.normal(message: "라이브러리 권한이 제한되어있습니다."))
+            case .denied:
+                self?.alert?(
+                    .ok(
+                        message: "갤러리 접근 권한이 거부되었습니다.",
+                        title: "권한 설정하기",
+                        handler: { _ in
+                            guard
+                                let settingURL = URL(string: UIApplication.openSettingsURLString),
+                                UIApplication.shared.canOpenURL(settingURL)
+                            else { return }
+                            UIApplication.shared.open(settingURL)
+                        }
+                    )
+                )
+            case .authorized:
+                var configuration = PHPickerConfiguration()
+                configuration.filter = .images
+                
+                let controller = PHPickerViewController(configuration: configuration)
+                controller.delegate = self
+                
+                self?.present?(.picker(controller))
+            case .limited:
+                fallthrough
+            @unknown default:
+                self?.alert?(.normal(message: "관리자에게 문의하세요"))
+            }
+        }
+    }
+}
+
+extension MainViewModel: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+
+        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                DispatchQueue.main.async {
+                    guard let self = self, let image = image as? UIImage else {
+                        debugPrint("could not load image", error?.localizedDescription ?? "")
+                        return
+                    }
+                    
+                    picker.dismiss(animated: true) {
+                        self.selectedImage?(image)
+                    }
+                }
+            }
+        }
+        
+    }
 }
