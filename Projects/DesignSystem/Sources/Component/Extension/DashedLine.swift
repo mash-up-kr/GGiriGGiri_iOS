7a6edd630c112073734afef164b2ba5c4e655a80
Eddy <kimkyunghun3@naver.com>
2022-07-26 23:01:46 +0900

    :recycle: DashedLine draw method 활용하도록 변경
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

--- Projects/DesignSystem/Sources/Component/Extension/DashedLine.swift
+++ Projects/DesignSystem/Sources/Component/Extension/DashedLine.swift
@@ -1,21 +1,21 @@
 //
-//  UIView+sugar.swift
+//  DashedLine.swift
 //  DesignSystem
 //
-//  Created by Eddy on 2022/07/24.
+//  Created by Eddy on 2022/07/26.
 //  Copyright © 2022 dvHuni. All rights reserved.
 //
 
 import UIKit
 
-extension UIView {
-    func createDottedLine(width: CGFloat, color: CGColor) {
+final class DashedLine: UIView {
+    override func draw(_ rect: CGRect) {
         let caShapeLayer = CAShapeLayer()
-        caShapeLayer.strokeColor = color
-        caShapeLayer.lineWidth = width
+        caShapeLayer.strokeColor = UIColor.black.cgColor
+        caShapeLayer.lineWidth = 1
         caShapeLayer.lineDashPattern = [3.5, 3.5]
         let cgPath = CGMutablePath()
-        let cgPoint = [CGPoint(x: self.bounds.minX, y: 0), CGPoint(x: 310, y: 0)]
+        let cgPoint = [CGPoint(x: 0, y: 0), CGPoint(x: self.frame.width, y: 0)]
         cgPath.addLines(between: cgPoint)
         caShapeLayer.path = cgPath
         layer.addSublayer(caShapeLayer)
