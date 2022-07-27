04337a40a58a55482e1341a32aeb218f0ab09e5d
sanghee <dev.sanghee@gmail.com>
2022-07-28 03:18:50 +0900

    :sparkles::bulb: MyBox enum, MyBoxEmptyView 추가 (#74)
    - MyBoxEmptyView의 configureEmptyCategory(with:) 메서드를 통해서 뷰의 내부 프로퍼티 변경

---
+++ Projects/App/Sources/MyBox/Model/MyBox.swift
@@ -0,0 +1,14 @@
+//
+//  MyBox.swift
+//  GGiriGGiri
+//
+//  Created by 안상희 on 2022/07/28.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import Foundation
+
+enum MyBox: CaseIterable {
+    case apply
+    case register
+}
