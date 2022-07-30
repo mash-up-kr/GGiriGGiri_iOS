dee534d01e90b20bfcb1ad828aac82725a5351f9
Eddy <kimkyunghun3@naver.com>
2022-07-30 16:53:24 +0900

    :bulb: CountdownCardViewStyle 구현
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

---
+++ Projects/DesignSystem/Sources/Component/CountdownCard/DDipCountdownCardViewStyle.swift
@@ -0,0 +1,19 @@
+//
+//  DDipCountdownCardViewStyle.swift
+//  DesignSystem
+//
+//  Created by Eddy on 2022/07/29.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+public struct DDipCountdownCardViewStyle {
+    public let iconImage: String
+    public let time: String
+    
+    public init(iconImage: String, time: String) {
+        self.iconImage = iconImage
+        self.time = time
+    }
+}
