eabb8ce611cca88537512cad8e9fa244cfba1d21
AhnSangHoon <ansh0324@gmail.com>
2022-08-01 13:49:09 +0900

    :recycle: Apply 화면 레이아웃 구조 변경

--- Projects/App/Sources/Apply/View/ApplyGifticonView.swift
+++ Projects/App/Sources/Apply/View/ApplyGifticonView.swift
@@ -11,20 +11,12 @@ import UIKit
 import DesignSystem
 import SnapKit
 
-protocol ApplyGifticonViewButtonDelegate: AnyObject {
-    func applyButtonTapped(completion: @escaping () -> Void)
-}
-
 /// 기프티콘 응모 뷰
 final class ApplyGifticonView: BaseView {
-    
-    weak var delegate: ApplyGifticonViewButtonDelegate?
-    
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.backgroundColor = .designSystem(.neutralWhite)
-        scrollView.showsVerticalScrollIndicator = false
-        return scrollView
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
     }()
     
     private let gifticonCardView = ApplyGifticonCardView()
@@ -37,80 +29,39 @@ final class ApplyGifticonView: BaseView {
         font: .designSystem(.pretendard, family: .bold, size: ._16)
     )
     
-    private let applyButton = TempButton(title: "지금 당장 응모할게요!")
-    
     override func setLayout() {
         super.setLayout()
+        addSubview(stackView)
         
-        scrollView.addSubviews(with: [gifticonCardView,
-                                      gifticonInfoView,
-                                      gifticonDDipInfoView,
-                                      infoLabel,
-                                      applyButton])
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
-        addSubviews(with: [scrollView, applyButton])
+        stackView.addArrangedSubviews(with: [
+            gifticonCardView,
+            gifticonInfoView,
+            gifticonDDipInfoView,
+            infoLabel
+        ])
         
         gifticonCardView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(393)
         }
         
         gifticonInfoView.snp.makeConstraints {
-            $0.top.equalTo(gifticonCardView.snp.bottom).offset(30)
-            $0.leading.trailing.equalTo(gifticonCardView)
             $0.height.equalTo(248)
         }
         
         gifticonDDipInfoView.snp.makeConstraints {
-            $0.top.equalTo(gifticonInfoView.snp.bottom).offset(24)
-            $0.leading.trailing.equalTo(gifticonCardView)
             $0.height.equalTo(104)
         }
         
         infoLabel.snp.makeConstraints {
-            $0.top.equalTo(gifticonDDipInfoView.snp.bottom).offset(28)
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(-37)
-        }
-        
-        scrollView.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(safeAreaLayoutGuide)
-            $0.bottom.equalTo(infoLabel.snp.bottom).offset(37)
-        }
-        
-        scrollView.contentLayoutGuide.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(safeAreaLayoutGuide)
-            $0.bottom.equalTo(infoLabel.snp.bottom).offset(37)
-        }
-        
-        applyButton.snp.makeConstraints {
-            $0.top.equalTo(scrollView.snp.bottom)
-            $0.leading.trailing.equalToSuperview().inset(16)
-            $0.height.equalTo(54)
-            $0.bottom.equalTo(safeAreaLayoutGuide)
+            $0.height.equalTo(100)
         }
     }
     
     override func configure() {
         super.configure()
-        
-        applyButton.isEnabled = true
-        
-        applyButton.setBackgroundColor(.designSystem(.secondaryBlue), for: .normal)
-        applyButton.setBackgroundColor(.designSystem(.secondarySkyblue200), for: .disabled)
-        
-        applyButton.setTitle("지금 당장 응모할게요!", for: .normal)
-        applyButton.setTitle("응모 완료!", for: .disabled)
-        
-        applyButton.layer.cornerRadius = 12
-        applyButton.clipsToBounds = true
-        applyButton.addTarget(self, action: #selector(applyButtonTapped(_:)), for: .touchUpInside)
-    }
-    
-    @objc private func applyButtonTapped(_ sender: UIButton) {
-        delegate?.applyButtonTapped(completion: {
-            self.applyButton.isEnabled = false
-        })
     }
 }
