918eaaaa2899d61ea80d26f5b09768f62c230179
Eddy <kimkyunghun3@naver.com>
2022-08-01 16:26:59 +0900

    :recycle: ListCardViewStyle status 추가
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

--- Projects/DesignSystem/Sources/Component/ListCard/DDIPListCardViewStyle.swift
+++ Projects/DesignSystem/Sources/Component/ListCard/DDIPListCardViewStyle.swift
@@ -9,23 +9,36 @@
 import Foundation
 
 public struct DDIPListCardViewStyle {
+    public enum ApplyTitleStatus: String {
+        case complete = "마감"
+        case result = "결과"
+        case empty = ""
+        
+        var description: String {
+            return self.rawValue
+        }
+    }
+
     public let brand: String
     public let name: String
     public let expirationDate: String
     public let iconImage: String
     public let description: String
+    public let applyStatus: ApplyTitleStatus
     
     public init(
         brand: String,
         name: String,
         expirationDate: String,
         iconImage: String,
-        description: String
+        description: String,
+        applyTitleStatus: ApplyTitleStatus = .empty
     ) {
         self.brand = brand
         self.name = name
         self.expirationDate = expirationDate
         self.iconImage = iconImage
         self.description = description
+        self.applyStatus = applyTitleStatus
     }
 }
