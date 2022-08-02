7ee561cfdb8fe632532ebaf0518b5b6e707e401a
Eddy <kimkyunghun3@naver.com>
2022-08-03 02:47:45 +0900

    :recycle: ListCardDeadlineView 로직 변경
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

--- Projects/DesignSystem/Sources/Component/ListCard/DDipListCardDeadlineView.swift
+++ Projects/DesignSystem/Sources/Component/ListCard/DDipListCardDeadlineView.swift
@@ -9,20 +9,12 @@
 import UIKit
 
 public class DDipListCardDeadlineView: DDipListCardApplyBaseView {
-    private let style: DDIPListCardViewStyle
-    
-    private func setDrawLabel() {
-        drawLabel.text = "\(style.applyStatus.description)까지 \(style.description)시간 남았어요!"
+    public func setListCardButton(buttonTitle: DDIPCardListButton.TitleStatus, buttonColor: UIColor?, isHidden: Bool) {
+        cardListButton.setButtonAttribute(titleStatus: buttonTitle, buttonColor: buttonColor, isHidden: isHidden)
+    }
+
+    public func setDrawLabel(titleStatus: String, leftTime: String) {
+        drawLabel.text = "\(titleStatus)까지 \(leftTime)시간 남았어요!"
         drawLabel.font = .designSystem(.pretendard, family: .regular, size: ._14)
-    }
-    
-    public init(style: DDIPListCardViewStyle) {
-        self.style = style
-        super.init(alarmButton: .init(style: .init(buttonColor: .designSystem(.secondaryBlue), title: .apply, isHidden: false)))
-        setDrawLabel()
-    }
-    
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
