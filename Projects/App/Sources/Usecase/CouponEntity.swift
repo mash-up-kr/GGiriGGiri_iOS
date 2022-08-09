92e76ac39e218ec694553b6126d447c1c79ba03f
Eddy <kimkyunghun3@naver.com>
2022-08-10 01:03:03 +0900

    :bulb: CouponDatum 추가
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

--- Projects/App/Sources/Usecase/CouponEntity.swift
+++ Projects/App/Sources/Usecase/CouponEntity.swift
@@ -8,20 +8,44 @@
 
 import Foundation
 
+import DesignSystem
+
 struct CouponEntity: Decodable {
     let code, message: String
     let data: [CouponDatum]
 }
 
-struct CouponDatum: Codable {
+struct CouponDatum: Decodable {
     let sprinkleID: Int
     let brandName, merchandiseName, category, expiredAt: String
     let participants: Int
     let sprinkleAt: String
-    let participateIn: Bool
+    let participateIn: Bool?
 
     enum CodingKeys: String, CodingKey {
         case sprinkleID = "sprinkleId"
         case brandName, merchandiseName, category, expiredAt, participants, sprinkleAt, participateIn
     }
+
+    var imageName: DDIPAsset.name {
+        switch category {
+        case "CAFE":
+            return .iconCafedesert
+        case "DELIVERY":
+            return .iconChicken
+        case "ICECREAM":
+            return .iconIcecream
+        case "CONVENIENCE_STORE":
+            return .iconDrugStore
+        case "FAST_FOOD":
+            return .iconFastfood
+        case "VOUCHER":
+            return .iconCoupon
+        case "ETC":
+            return .iconGuitar
+        default:
+            return .iconLogoCharacter
+        }
+    }
+
 }
