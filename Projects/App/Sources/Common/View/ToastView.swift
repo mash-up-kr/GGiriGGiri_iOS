ae5d1a4ecd2ec2cfd505a4d2df660f6a9c1ac53a
Eddy <kimkyunghun3@naver.com>
2022-08-10 22:28:32 +0900

    :recycle: ToastView 필요한 메서드 추가
    # :tada: = 프로젝트 시작 (#)
    # :sparkles: = create 파일 생성 시 (#)
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시 (#)
    # :rocket: = release 배포 시 (#)
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우) (#)
    # :memo: = 문서 작성 시 (#)
    # :art: = 디자인 시스템 관련 내용 (Color / Asset) (#)
    # :hammer: = 테스트 코드 작성 (#)
    ################
    
    # 커밋 타입 : 커밋 제목 (계정명/저장소명#이슈번호) 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성

--- Projects/App/Sources/Common/View/ToastView.swift
+++ Projects/App/Sources/Common/View/ToastView.swift
@@ -15,29 +15,41 @@ final class ToastView {
     private let toastView = DDIPToastView()
     private let wrapperView = UIView()
     private let dimView = UIView()
-    
+
+    func configureToastView(with view: UIView, title: String, description: String, image: DDIPAsset.name) {
+        toastView.setTitleLabel(title)
+        toastView.setDescriptionLabel(description)
+        toastView.setIconImageView(image)
+
+        setToastView(view: view)
+    }
+
     func configureToastView(with view: UIView, style: DDIPToastView.ToastViewOptions, image: DDIPAsset.name) {
         toastView.setTitleLabel(style)
         toastView.setDescriptionLabel(style)
         toastView.setIconImageView(image)
-        
+
+        setToastView(view: view)
+    }
+
+    private func setToastView(view: UIView) {
         dimView.backgroundColor = .designSystem(.neutralBlack)?.withAlphaComponent(0.25)
         dimView.addSubview(toastView)
         wrapperView.addSubview(dimView)
-        
+
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissPopupView(_:)))
         wrapperView.addGestureRecognizer(tapGesture)
         wrapperView.isUserInteractionEnabled = true
-        
+
         view.addSubview(wrapperView)
         wrapperView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        
+
         dimView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        
+
         toastView.snp.makeConstraints {
             $0.width.equalTo(289)
             $0.height.equalTo(295)
@@ -46,7 +58,7 @@ final class ToastView {
 
         toastView.alpha = 0
     }
-    
+
     func showToastView(with view: UIView) {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()) {
             UIView.transition(with: view, duration: 0.3, options: [.curveEaseInOut], animations: {
