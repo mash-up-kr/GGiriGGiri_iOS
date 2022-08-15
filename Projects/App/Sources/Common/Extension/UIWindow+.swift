3224b73394ab1e6573499d86134ca1ab8d54b9c9
Sanghee <dev.sanghee@gmail.com>
2022-08-15 22:30:05 +0900

    :sparkles: RootViewController를 replace하는 메서드 추가 (#143)

---
+++ Projects/App/Sources/Common/Extension/UIWindow+.swift
@@ -0,0 +1,49 @@
+//
+//  UIWindow+.swift
+//  GGiriGGiri
+//
+//  Created by 안상희 on 2022/08/15.
+//  Copyright © 2022 dvHuni. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+    func replaceRootViewController(_ replacementViewController: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        let snapshotImageView = UIImageView(image: self.snapshot())
+        self.addSubview(snapshotImageView)
+        
+        let dismissCompletion = { () -> Void in
+            self.rootViewController = replacementViewController
+            self.bringSubviewToFront(snapshotImageView)
+            
+            if animated {
+                UIView.animate(withDuration: 0.4, animations: { () -> Void in
+                    snapshotImageView.alpha = 0
+                }, completion: { success -> Void in
+                    snapshotImageView.removeFromSuperview()
+                    completion?()
+                })
+            } else {
+                snapshotImageView.removeFromSuperview()
+                completion?()
+            }
+        }
+        
+        if self.rootViewController!.presentedViewController != nil {
+            self.rootViewController!.dismiss(animated: true, completion: dismissCompletion)
+        } else {
+            dismissCompletion()
+        }
+    }
+    
+    func snapshot() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
+        drawHierarchy(in: bounds, afterScreenUpdates: true)
+        
+        guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage.init() }
+        UIGraphicsEndImageContext()
+        
+        return result
+    }
+}
